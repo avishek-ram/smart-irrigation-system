69bf0f475feb74600b110cd46cafc441651cc348
Avishek Anishkar Ram <48897816+avishek-ram@users.noreply.github.com>
2022-10-30 02:33:16 +1200

    sample web app added

---
+++ smart_irrigation_web_app/smart_irrigation_web_app/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace smart_irrigation_web_app.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
